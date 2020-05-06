a1e46d5f3d61bd8e0708a33abc1c49571f44a748
i007c <dr007cc@gmail.com>
2020-05-06 11:26:19 +0430

    line 4 for test

--- HaumeaBot.py
+++ HaumeaBot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Programmer 007
+# this line 4 test
 
 import telegram
 from telegram import ChatAction, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
